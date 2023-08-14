e0a092f05efb25114720ff9e24b23e38e56e5bed
Tien Dinh N <tien.dinh@supremetech.vn>
2023-08-14 21:39:08 +0700

    Fix ex10

--- javascript-exercise/ex10.js
+++ javascript-exercise/ex10.js
@@ -1,33 +1,34 @@
 /**
  * 10. Cho biết kết quả in ra của đoạn code sau.
 */
-console.log('stack [1]'); // Đoạn 1 sẽ vào stack đầu tiên và thực thi đầu tiên
+console.log('stack [1]'); // Đoạn 4 sẽ vào stack đầu tiên và thực thi đầu tiên
 setTimeout(function A() {
   console.log('macro [2]');
-}, 0); // Đoạn 2, 3, 4 sẽ đưa cho webApis xử lý để đưa vào CallbackQueue (macroTask)
+}, 0); // Đoạn 5, 6, 7 sẽ đưa cho webApis xử lý để đưa vào CallbackQueue (macroTask)
 setTimeout(function B() {
   console.log('macro [3]');
-}, 0); // Đoạn 5, 6, 7 sẽ đưa cho webApis xử lý để đưa vào CallbackQueue (macroTask) sau đoạn 2, 3, 4
+}, 0); // Đoạn 8, 9, 10 sẽ đưa cho webApis xử lý để đưa vào CallbackQueue (macroTask) sau đoạn 5, 6, 7
 const p = Promise.resolve();
+// Đoạn 13 sẽ đưa cho webApis xử lý để đưa vào CallbackQueue (microTask) và thực thi trước 2 cái setTimeout đoạn 5 và 8 phía trên
 p.then(function C() {
-  // Đoạn 9 sẽ đưa cho webApis xử lý để đưa vào CallbackQueue (microTask) và thực thi trước 2 cái setTimeout đoạn 2 và 5 phía trên
-  // Đoạn 11 sẽ đưa cho webApis xử lý để đưa vào CallbackQueue (microTask) thực thi trước 2 macroTask đoạn 2, 3, 4, 5, 6, 7
+  // Đoạn 15 sẽ đưa cho webApis xử lý để đưa vào CallbackQueue (macroTask) thực thi trước 2 macroTask đoạn 5, 6, 7, 8, 9, 10
   setTimeout(function D() {
-    // Đoạn 13 sẽ đưa vào callback và thực thi sau đoạn 23
+    // Đoạn 17 sẽ đưa vào stack và thực thi sau đoạn 26
     console.log('macro [4]');
+    // Đoạn 19, 20, 21 sẽ đưa cho webApis xử lý để đưa vào CallbackQueue (macroTask) thực thi sau đoạn 22, 23, 24
     setTimeout(function E() {
-      // Đoạn 14, 15, 16 sẽ đưa cho webApis xử lý để đưa vào CallbackQueue (microTask) thực thi sau đoạn 17, 18, 19
       console.log('macro [5]');
     });
+    // Đoạn 23, 24, 25, 26 là microTask nên thực thi trước đoạn 19, 20, 21 (macroTask)
     p.then(function F() {
-      console.log('micro [6]'); // Đoạn 17, 18, 19, 20 là microTask nên thực thi trước đoạn 14, 15, 16 (macroTask)
+      console.log('micro [6]'); 
     });
   }, 0);
-  console.log('micro [7]'); // Đoạn 21 sẽ vào callback nên thực thi trước đoạn 11
+  console.log('micro [7]'); // Đoạn 26 sẽ vào stack nên thực thi trước đoạn 15
 });
 
-// Sau khi thực thi xong p.then() thì đoạn 2, 3, 4, 5, 6, 7 sẽ tiếp tục thực thi
-console.log('stack [8]'); // Đoạn 25 sẽ thực thi sau đoạn 1
+// Sau khi thực thi xong p.then() thì đoạn 5, 6, 7, 8, 9, 10 sẽ tiếp tục thực thi
+console.log('stack [8]'); // Đoạn 31 sẽ thực thi sau đoạn 1
 
 /* 
 Thứ tự thực thi
