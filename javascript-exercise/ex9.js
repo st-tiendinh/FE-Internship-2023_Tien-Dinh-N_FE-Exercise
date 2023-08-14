1f1283e0d97079bae01f6ef767783b70431f55bf
Tien Dinh N <tien.dinh@supremetech.vn>
2023-08-14 21:28:13 +0700

    Fix comment ex9

--- javascript-exercise/ex9.js
+++ javascript-exercise/ex9.js
@@ -2,6 +2,9 @@
  * 9.Write a JavaScript program that creates a class `Book` with properties for title, author, and publication year. Include a method
  * to display book details. Create a subclass called 'Ebook' that inherits from the 'Book' class and includes an additional property for
  * book price. Override the display method to include the book price. Create an instance of the 'Ebook' class and display its details.
+ */
+
+/**
  * Represents a Book with title, author and publication year.
  * @class
  */
